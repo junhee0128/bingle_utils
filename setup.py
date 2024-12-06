c7ebfae7e0cc29f061a60d82a734dc3d5c7f69f8
junhee <junhee0128@gmail.com>
2024-12-06 10:28:37 +0900

    [version 0.1.3] - releases 'AI Caller'. - added 'API Client' for bingle.utils. - added a demo of 'AI Call Log Handler'. - corrected import setting of '__init__.py' files.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bingle',
-    version='0.1.2',
+    version='0.1.3',
     author='junhee',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -10,6 +10,7 @@ setup(
     install_requires=[
         'pandas>=2.2.3',
         'pyarrow>=18.1.0',
-        'numpy>=1.26.2'
+        'numpy>=1.26.2',
+        'requests>=2.32.3'
     ]
 )
