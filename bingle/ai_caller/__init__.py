05e77e972da50a0f2928ac18ac019f4a09010ab1
junhee <junhee0128@gmail.com>
2024-12-13 21:39:47 +0900

    [version 0.1.5] - added 'copy' function to the 'Messages' class of the ai prompter. - renamed 'AICallDataFormatter' to 'OpenAIDataFormatter'. (added 'DataclassToJsonschemaConverter')

--- bingle/ai_caller/__init__.py
+++ bingle/ai_caller/__init__.py
@@ -1,3 +1,3 @@
 from .ai_caller import AICaller
 from .src.call_object import AICall, AICallSummary, AICallUsage, AICallPrompt, AICallCompletion
-from .src.call_data_formatter import AICallDataFormatter
+from .src.openai_dataformatter import OpenAIDataFormatter
