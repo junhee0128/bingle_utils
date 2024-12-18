e68b29b3a1c126a609fd759f5f97558aeadad913
junhee <junhee0128@gmail.com>
2024-12-18 22:18:39 +0900

    [version 0.1.5] - added 'copy' function to the 'Content', 'Prompt', and 'Messages' classes of the ai prompter. - renamed 'AICallDataFormatter' to 'OpenAIDataFormatter'. (added 'DataclassToJsonschemaConverter') - updated api_version in the default api spec file for azure openai to support structured output. - added retry decorator for 'AICaller'

---
+++ test/test_tenacity.py
@@ -0,0 +1,30 @@
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
+
+
+# Retry Decorator 정의
+def retry_on_exception(max_attempts=10, wait_time=2):
+    """
+    함수 호출에 실패했을 때, 재시도하는 데코레이터 함수.
+    :param max_attempts: 최대 재시도 횟수
+    :param wait_time: 재시도 간 대기 시간 (지수적 증가 사용)
+    """
+    return retry(
+        reraise=True,  # 마지막 exception을 그대로 raise
+        stop=stop_after_attempt(max_attempts),  # 최대 재시도 횟수
+        wait=wait_exponential(multiplier=wait_time, min=wait_time, max=60),  # 지수적 증가 대기
+        retry=retry_if_exception_type(Exception),  # 재시도할 예외 타입
+    )
+
+
+@retry_on_exception(max_attempts=2, wait_time=2)
+def f():
+    print("Attempting to execute...")
+    return 3 / 0
+
+
+if __name__ == '__main__':
+    try:
+        f()
+    except Exception as e:
+        print(f"Exception after retries: {e}")
+    print("Execution complete.")
