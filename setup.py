e99bb72997abaaa94f67fe16189d0f6ead46b1bf
junhee <junhee0128@gmail.com>
2025-05-17 09:21:00 +0900

    - FileProcessor.save_json 인코딩 예외상황 대응코드 강화

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bingle',
-    version='0.1.9',
+    version='0.1.10',
     author='junhee',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
