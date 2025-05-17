a9c13b807a71cb5901e6e0533e6165cbcb739ac6
junhee <junhee0128@gmail.com>
2025-05-17 09:20:38 +0900

    - FileProcessor.save_json 인코딩 예외상황 대응코드 강화

--- bingle/utils/file_processor.py
+++ bingle/utils/file_processor.py
@@ -77,8 +77,8 @@ class FileProcessor:
     def save_json(filepath: PathLike, obj: dict):
         if not os.path.exists(filepath):
             os.makedirs(os.path.dirname(filepath), exist_ok=True)
-        with open(filepath, 'w') as f:
-            json.dump(obj, f)
+        with open(filepath, 'w', encoding='utf-8') as f:
+            json.dump(obj, f, ensure_ascii=False)
 
     @staticmethod
     def save_pickle(filepath: PathLike, data: Any):
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bingle',
-    version='0.1.8',
+    version='0.1.9',
     author='junhee',
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
